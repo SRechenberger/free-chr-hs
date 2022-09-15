58b737d4d3e80f5c1dc66ac80f3da0ee5f097351
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2022-09-15 13:30:09 +0200

    first working execution

--- free-chr/src/CHR/Execution/Generic.hs
+++ free-chr/src/CHR/Execution/Generic.hs
@@ -4,6 +4,8 @@ module CHR.Execution.Generic where
 
 import Data.Maybe (isJust)
 import Data.List (subsequences, permutations)
+import Data.Foldable.Extra (findM)
+import Data.Bifunctor (bimap)
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -17,6 +19,8 @@ import Control.Monad.Except
 import Control.Lens
 
 import Control.Arrow
+import Control.Applicative (liftA2)
+
 
 data CHRState c = CHRState
   { _nextId      :: Int
@@ -28,8 +32,26 @@ data CHRState c = CHRState
 
 makeLenses ''CHRState
 
+
+newCHRState :: CHRState c
+newCHRState = CHRState
+  { _nextId      = 0
+  , _constraints = Map.empty
+  , _alive       = Set.empty
+  , _history     = Set.empty
+  }
+
+
 newtype Solver m c = Solver
   { runSolver :: Int -> c -> CHRState c -> m (Maybe ([m [c]], CHRState c)) }
+
+
+instance Monad m => Semigroup (Solver m c) where
+  f <> g = Solver { runSolver = solve }
+    where
+      solve i c s = do
+        rf <- runSolver f i c s
+        maybe (runSolver g i c s) (pure >>> pure) rf
 
 
 fresh :: CHRState c -> (Int, CHRState c)
@@ -48,12 +70,20 @@ kill i = alive %~ Set.delete i
   >>> constraints %~ Map.delete i
 
 
+isAlive :: Int -> CHRState c -> Bool
+isAlive i = (^. alive . to (elem i))
+
+
+kills :: [Int] -> CHRState c -> CHRState c
+kills is s = foldr kill s is
+
+
 record :: String -> [Int] -> CHRState c -> CHRState c
 record r is = history %~ Set.insert (r, is)
 
 
 check :: String -> [Int] -> CHRState c -> Bool
-check r is = (^. history . to (Set.member (r, is)))
+check r is = (^. history . to (Set.member (r, is))) >>> not
 
 
 match :: Int -> a -> [a -> Bool] -> [(Int, a)] -> [[(Int, a)]]
@@ -66,15 +96,53 @@ match i c ps as = [ as'' |
 
 
 rule :: (Monad m)
-     => [c -> Bool] -> [c -> Bool]
+     => String
+     -> [c -> Bool] -> [c -> Bool]
      -> ([c] -> m Bool)
      -> ([c] -> [m [c]])
      -> Solver m c
-rule kept removed guard body = Solver { runSolver = solver }
+rule name kept removed guard body = Solver { runSolver = solver }
   where
     solver i c state = do
-      matchings <- filterM (map snd >>> guard)
+      matching <- findM
+        (unzip
+        >>> bimap (\is -> pure $ check name is state) guard
+        >>> uncurry (liftA2 (&&)))
         $ match i c (kept <> removed) (Map.toList $ state^.constraints)
-      case matchings of
-        []  -> pure Nothing
-        cs:_ -> undefined
+      pure $ do
+        m <- matching
+        let (is, cs) = unzip m
+        let rs = drop (length kept) is
+        let state' = state
+                    & kills rs
+                    & (if length rs > 0 then id else record name is)
+        pure (body cs, state')
+
+
+(<.>) :: Monad m => Solver m c -> Solver m c -> Solver m c
+(<.>) = (<>)
+
+run :: Monad m => Solver m c -> [c] -> m (CHRState c)
+run solver query = run' solver query newCHRState
+
+run' :: Monad m => Solver m c -> [c] -> CHRState c -> m (CHRState c)
+run' _      []     state = pure state
+run' solver (c:cs) state = do
+  let (i, state') = fresh state
+  call solver i c (add i c state') >>= run' solver cs
+
+
+call :: Monad m => Solver m c -> Int -> c -> CHRState c -> m (CHRState c)
+call solver i c state
+  | isAlive i state = do
+    mr <- runSolver solver i c state
+    case mr of
+      Nothing            -> pure state
+      Just (mqs, state') -> do
+        state'' <- foldM
+          (\s mq -> join $ run' solver <$> mq <*> pure s)
+          state'
+          mqs
+        (if isAlive i state'' then call solver i c else pure) state''
+
+  | otherwise = pure state
